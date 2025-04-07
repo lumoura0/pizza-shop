1e44ce0ef15100463b20764eff336fb7f42c0934
Luis Fernando <luisfernando.mourag@gmail.com>
2025-04-07 16:37:47 -0300

    Criando Loading das métricas

--- src/pages/app/dashboard/month-orders-amount.tsx
+++ src/pages/app/dashboard/month-orders-amount.tsx
@@ -2,6 +2,7 @@ import { getMonthOrdersAmount } from '@/api/get-month-orders-amount';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useQuery } from '@tanstack/react-query';
 import { Utensils } from 'lucide-react';
+import { MetricCardSkeleton } from './metric-card-skeleton';
 
 export function MonthOrdersAmountCard() {
 
@@ -17,7 +18,7 @@ export function MonthOrdersAmountCard() {
                 <Utensils className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent className='space-y-1'>
-                {monthOrdersAmount && (
+                {monthOrdersAmount ? (
                     <>
                         <span className="text-2xl font-bold tracking-tight">
                             {monthOrdersAmount.amount.toLocaleString('pt-BR')}
@@ -40,6 +41,8 @@ export function MonthOrdersAmountCard() {
                             )}
                         </p>
                     </>
+                ) : (
+                    <MetricCardSkeleton />
                 )}
             </CardContent>
         </Card>
